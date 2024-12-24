5dc37bd1d5b4df1817843610311b54fe9ca44275
Muhammad Hammad Yousaf <hammad.yousaf@arbisoft.com>
2024-12-24 13:26:24 +0500

    fix: fix lint and test issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { injectIntl } from '@edx/frontend-platform/i18n';
 import { sendTrackEvent } from '@edx/frontend-platform/analytics';
-import { ensureConfig, getConfig } from '@edx/frontend-platform';
+import { ensureConfig } from '@edx/frontend-platform';
 import { AppContext } from '@edx/frontend-platform/react';
-
-import messages from './Footer.messages';
-import LanguageSelector from './LanguageSelector';
 
 ensureConfig([
   'LMS_BASE_URL',
@@ -34,33 +30,13 @@ class SiteFooter extends React.Component {
   }
 
   render() {
-    const {
-      supportedLanguages,
-      onLanguageSelected,
-      logo,
-      intl,
-    } = this.props;
-    const showLanguageSelector = supportedLanguages.length > 0 && onLanguageSelected;
-    const config = getConfig();
-
     return (
-      <div className="wrapper wrapper-footer">
-      </div>
+      <div className="wrapper wrapper-footer" />
     );
   }
 }
 
 SiteFooter.contextType = AppContext;
-
-SiteFooter.propTypes = {
-  intl: intlShape.isRequired,
-  logo: PropTypes.string,
-  onLanguageSelected: PropTypes.func,
-  supportedLanguages: PropTypes.arrayOf(PropTypes.shape({
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-  })),
-};
 
 SiteFooter.defaultProps = {
   logo: undefined,
